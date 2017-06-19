003052a5f609ce53d528c545b97949681d5ce6d5
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-06-19 16:03:45 -0400

    Got jQuery working with electron.
    (had to use a special method of defining window.$ variable to get jQuery working).

--- bit-freeze/lib/walletGenerator.js
+++ bit-freeze/lib/walletGenerator.js
@@ -1,10 +1,11 @@
 //Write logic for generating a new k/v pair in here
-const $ = require('jquery')
+//const $ = require('jquery')
+const BTC = require('bitcoinjs-lib')
 
 $(document).ready( () => {
   console.log('document ready!');
 
-  $('.BTC-btn-wallet-gen').on('click', (e) => {
+  $('.btc-btn-generate').on('click', (e) => {
     //doesnt work yet- next step add bootstrap and refactor btn HTML.
     console.log('button wuz click\'d');
   })
