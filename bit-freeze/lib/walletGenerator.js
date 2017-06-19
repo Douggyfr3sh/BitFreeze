ff2224850d60d7300547a76ffc09a5f151435637
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-06-19 16:28:40 -0400

    Implemented working public/private key generation  on button press.

--- bit-freeze/lib/walletGenerator.js
+++ bit-freeze/lib/walletGenerator.js
@@ -8,5 +8,16 @@ $(document).ready( () => {
   $('.btc-btn-generate').on('click', (e) => {
     //doesnt work yet- next step add bootstrap and refactor btn HTML.
     console.log('button wuz click\'d');
+    let newWallet = BTC.ECPair.makeRandom();
+    let public = newWallet.getAddress();
+    let _private = newWallet.toWIF();
+    let shownPrivate = _private;
+
+    //generate hidden private string:
+
+    //show results
+    $('.btc-public-key').text(public);
+    $('.btc-private-key').text(shownPrivate);
+
   })
 });
